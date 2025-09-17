7da4977bab6a5e463308a4f455854ba14603fbcc
ponparkavi <ponparkavi0818@gmail.com>
2025-09-18 02:03:04 +0530

    Calendar added

--- src/CalendarDashboard.jsx
+++ src/CalendarDashboard.jsx
@@ -4,7 +4,7 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
-import "./App.css";
+import "./CalendarDashboard.css";
 
 export default function CalendarDashboard() {
   const calendarRef = useRef(null);
